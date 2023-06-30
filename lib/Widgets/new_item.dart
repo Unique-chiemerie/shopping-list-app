f687a37562c27f0a5c3d7f394724e492bb6adb8d
Unique-chiemerie <lilanimate1@gmail.com>
2023-06-30 21:00:09 +0100

    fire base for storing data

--- lib/Widgets/new_item.dart
+++ lib/Widgets/new_item.dart
@@ -18,12 +18,12 @@ class _NewItemState extends State<NewItem> {
   var _enteredName = '';
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
       final url = Uri.https(
           'udemyback-default-rtdb.firebaseio.com', 'shopping-list.json');
-      http.post(
+      final response = await http.post(
         url,
         headers: {
           'Content-Type': 'application/json',
@@ -36,6 +36,14 @@ class _NewItemState extends State<NewItem> {
           },
         ),
       );
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop();
     }
   }
 
