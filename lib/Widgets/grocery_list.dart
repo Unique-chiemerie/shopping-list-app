45b4422cc5e999722df869ff1a0b2b8cb4ff75fb
Unique-chiemerie <lilanimate1@gmail.com>
2023-07-01 02:03:32 +0100

    incase of error

--- lib/Widgets/grocery_list.dart
+++ lib/Widgets/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -27,6 +28,13 @@ class _GroceryListState extends State<GroceryList> {
     final url = Uri.https(
         'udemyback-default-rtdb.firebaseio.com', 'shopping-list.json');
     final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data . please try again later';
+      });
+    }
+
+    print(response.statusCode);
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -102,7 +110,11 @@ class _GroceryListState extends State<GroceryList> {
         ),
       );
     }
-
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         actions: [
